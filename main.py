82dc32c2af37ecf22ee411f96d0d48b7de90f2ad
Ominda Rathnasiri <ominda.rathnasiri@dialog.lk>
2023-08-28 15:56:13 +0530

    add additional functions

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home():
     return "Hello, this is the main page <h3> Hello! </h3>"
 
+@app.route("/products")
+def products():
+    return "Hello, this is the <h3> Products! </h3> page!"
+
+@app.route("/contactus")
+def contactUs():
+    return "Hello, this is the <h3> Contact Us! </h3> page!"
+
 @app.route("/<name>")
 def user(name):
     return f"Hello {name}!"
